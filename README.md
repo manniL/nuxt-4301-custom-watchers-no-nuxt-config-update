01873db94fc119bbc940bec9a05a059c5aec3071
Alexander Lichter <manniL@gmx.net>
2019-01-08 18:49:46 +0000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# nuxt-4301-custom-watchers-no-nuxt-config-update
-Created with CodeSandbox
---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+import head from "./config/head";
+import env from "./config/env";
+export default {
+  watch: ["~/config/*.js"],
+  head,
+  env
+};
